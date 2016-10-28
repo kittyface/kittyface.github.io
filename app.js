4f8d596ab51b8f914e17207f7e60c68668ac67b6
Robert <brandr2@uw.edu>
2016-10-28 13:01:14 -0700

    fixed require http

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require(['express']);
 var app = express();
-var serv = require(['http']).Server(app);
+var http = require(['http'])
+var serv = http.Server(app);
  
 app.get('/',function(req, res) {
     res.sendFile(__dirname + '/client/index.html');
