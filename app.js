882381b8d78f424f73bca0d0dab8ffe465102d97
Robert <brandr2@uw.edu>
2016-10-28 12:57:38 -0700

    fixed some more require calls

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-var express = require('express');
+var express = require(['express']);
 var app = express();
-var serv = require('http').Server(app);
+var serv = require(['http']).Server(app);
  
 app.get('/',function(req, res) {
     res.sendFile(__dirname + '/client/index.html');
